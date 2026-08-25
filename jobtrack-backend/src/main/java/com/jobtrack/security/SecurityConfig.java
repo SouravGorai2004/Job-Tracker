340c4e73c237c7461a951d5774b4a1c921827d57
SouravGorai2004 <sourav2023.sg@gmail.com>
2026-08-25 15:41:47 +0530

    introduce health check

--- jobtrack-backend/src/main/java/com/jobtrack/security/SecurityConfig.java
+++ jobtrack-backend/src/main/java/com/jobtrack/security/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**","/health-check").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider())
